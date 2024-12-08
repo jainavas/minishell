4a602dfec0e118fbafa8a8d1bdf4e032719c20c9
mpenas-z <mpenas-z@student.42madrid.com>
2024-12-08 15:20:19 +0100

    Clarify .h files

--- inc/pipex.h
+++ inc/pipex.h
@@ -6,7 +6,7 @@
 /*   By: jainavas <jainavas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 17:22:26 by jainavas          #+#    #+#             */
-/*   Updated: 2024/12/08 15:02:21 by mpenas-z         ###   ########.fr       */
+/*   Updated: 2024/12/08 15:19:42 by mpenas-z         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,22 +48,26 @@ typedef struct pipex
 	struct mini	*mini;
 }	t_pipex;
 
-void		freepipex(t_pipex *vars);
-char		*pathseek(char **args, char **envp);
-void		closeanddupoutput(int fd[2]);
+/* pipex.c */
 void		closeanddupinput(int fd[2]);
-int			checks(char **argv, t_pipex *var);
-void		fdtofile(t_pipex *var, char *filename);
 int			lastcmdcall(t_pipex *var, char **cmd, char *path);
-int			normalcmdcall(t_pipex *var, char **cmd, char *path);
 int			firstcmdcall(t_pipex *var, char **cmd, char *path);
 int			vardefs(t_pipex *vars, char **argv, int argc);
+/* pipex2.c */
+void		freepipex(t_pipex *vars);
+char		*pathseek(char **args, char **envp);
+int			checks(char **argv, t_pipex *var);
+void		fdtofile(t_pipex *var, char *filename);
+void		closeanddupoutput(int fd[2]);
+/* pipex3.c */
 void		triplepointeralloc(t_pipex *vars, int argc);
 int			checkpaths(t_pipex *vars);
+int			normalcmdcall(t_pipex *var, char **cmd, char *path);
 void		limmitator(char *lim, int fdin);
+/* pipex4.c */
 int			limvardefs(t_pipex *vars, char **argv, int argc);
 char		*pwdseek(t_pipex *var);
 int			limornot(int argc, char **argv, t_pipex *vars);
-void		anyfdtofile(int fd, char *filename, int app);
+// void		anyfdtofile(int fd, char *filename, int app);
 
 #endif
